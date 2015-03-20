e9c9011f3564e7be72b6dc58b7bb50444e7fe61b
masafumi terazono <hijirinhijirin@gmail.com>
2015-03-20 11:25:18 +0900

    Lesson35,Lesson36,Lesson37,Lesson38を追加

--- Chapter08/Lesson37/app/src/main/java/com/yokmama/learn10/chapter08/lesson37/MainActivity.java
+++ Chapter08/Lesson37/app/src/main/java/com/yokmama/learn10/chapter08/lesson37/MainActivity.java
@@ -1,9 +1,14 @@
 package com.yokmama.learn10.chapter08.lesson37;
 
+import android.graphics.Color;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -18,21 +23,27 @@ import org.json.JSONObject;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = MainActivity.class.getSimpleName();
     private RequestQueue mQueue;
+    private LinearLayout mColorsLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mColorsLayout = (LinearLayout)findViewById(R.id.colorsLayout);
         mQueue = Volley.newRequestQueue(this);
 
         loadColor();
     }
 
     private void loadColor(){
+        Log.d(TAG, "loadColor");
+        mColorsLayout.removeAllViews();
+
         //接続先
-        String url = "https://github.com/yokmama/learn10_2015/tree/develop/samples/colors.json";
+        String url = "https://raw.githubusercontent.com/yokmama/learn10_2015/develop/samples/colors.json";
 
         //キューにリクエストを追加
         mQueue.add(new JsonObjectRequest(
@@ -42,14 +53,36 @@ public class MainActivity extends ActionBarActivity {
 
                     @Override
                     public void onResponse(JSONObject response) {
+                        Log.d(TAG, response.toString());
+                        try {
+                            JSONArray colorsArray = response.getJSONArray("colorsArray");
+                            for(int i=0; i<colorsArray.length(); i++){
+                                JSONObject colorObject = colorsArray.getJSONObject(i);
+                                addItem(colorObject.getString("colorName"), colorObject.getString("hexValue"));
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
 
                     }
                 },
                 new Response.ErrorListener(){
                     @Override
                     public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, error.toString());
 
                     }
                 }));
+    }
+
+    private void addItem(String colorName, String hexValue){
+        Log.d(TAG, colorName + "," + hexValue);
+        TextView item = (TextView)getLayoutInflater().inflate(R.layout.color_row, null, false);
+
+        item.setText(colorName);
+        item.setBackgroundColor(Color.parseColor(hexValue));
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        mColorsLayout.addView(item, params);
     }
 }
