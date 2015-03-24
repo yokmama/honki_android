92d4e86c6d1e76ddf5d5d639b23978246a26ff41
satoshi.kohno <s_kohno@kayosystem.com>
2015-03-24 11:46:05 +0900

    Lesson25: amendでconflictさせてしまったので修正

--- Chapter06/Lesson25/app/src/main/java/com/yokmama/learn10/chapter06/lesson25/MainActivity.java
+++ Chapter06/Lesson25/app/src/main/java/com/yokmama/learn10/chapter06/lesson25/MainActivity.java
@@ -1,44 +1,29 @@
 package com.yokmama.learn10.chapter06.lesson25;
 
-<<<<<<< HEAD
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
 import android.text.TextUtils;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.view.WindowManager;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
-=======
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
->>>>>>> e4fd72a43cc7b855ec9230750fefc3f8b9606d1b
 
 
 public class MainActivity extends ActionBarActivity {
 
-<<<<<<< HEAD
     private EditText mEditText;
     private ListView mListView;
 
-=======
->>>>>>> e4fd72a43cc7b855ec9230750fefc3f8b9606d1b
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
 
         mEditText = (EditText) findViewById(R.id.edit);
         mListView = (ListView) findViewById(android.R.id.list);
@@ -127,30 +112,5 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return true;
-=======
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
->>>>>>> e4fd72a43cc7b855ec9230750fefc3f8b9606d1b
     }
 }
