646a13ac699dc2339dedc22330b221a6f7e4175c
satoshi.kohno <s_kohno@kayosystem.com>
2015-03-25 13:13:40 +0900

    Lesson25途中

--- Chapter06/Lesson25/app/src/main/java/com/yokmama/learn10/chapter06/lesson25/MainActivity.java
+++ Chapter06/Lesson25/app/src/main/java/com/yokmama/learn10/chapter06/lesson25/MainActivity.java
@@ -5,6 +5,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.AlarmClock;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.WindowManager;
@@ -15,15 +16,25 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity implements View.OnClickListener {
 
     private EditText mEditText;
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mToolbar = (Toolbar) findViewById(R.id.tool_bar);
         mEditText = (EditText) findViewById(R.id.edit);
         findViewById(R.id.button1).setOnClickListener(this);
         findViewById(R.id.button2).setOnClickListener(this);
+        findViewById(R.id.button3).setOnClickListener(this);
+        findViewById(R.id.button4).setOnClickListener(this);
+        findViewById(R.id.button5).setOnClickListener(this);
+
+        // Toolbarの設定
+        setSupportActionBar(mToolbar);
+
+        mToolbar.setTitle(getTitle());
 
         // キーボードを表示させる
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
@@ -36,6 +47,8 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
         int id = v.getId();
         if (id == R.id.button1) {
             // Google検索
+            if (checkEmpty(keyword)) return;
+
             // https://www.google.co.jp/search?q=android&hl=ja のようなURLを構築する
             Uri uri = Uri.parse("https://www.google.co.jp/search?hl=ja")
                     .buildUpon().appendQueryParameter("q", keyword).build();
@@ -44,10 +57,42 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         } else if (id == R.id.button2) {
             // Youtube検索
+            if (checkEmpty(keyword)) return;
+
             Intent intent = new Intent(Intent.ACTION_SEARCH);
             intent.setPackage("com.google.android.youtube");
             intent.putExtra("query", keyword);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+        } else if (id == R.id.button3) {
+            // Playストア検索
+            if (checkEmpty(keyword)) return;
+
+            Uri uri = Uri.parse("https://play.google.com/store/search")
+            .buildUpon().appendQueryParameter("q", keyword).build();
+            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+            startActivity(intent);
+
+        } else if (id == R.id.button4) {
+            // アラーム設定
+            if (checkEmpty(keyword)) return;
+
+            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
+            intent.putExtra(AlarmClock.EXTRA_MESSAGE, keyword);
+            // 時間を予め適当に指定
+            intent.putExtra(AlarmClock.EXTRA_HOUR, 8); // 時
+            intent.putExtra(AlarmClock.EXTRA_MINUTES, 5); // 分
+            startActivity(intent);
+
+        } else if (id == R.id.button5) {
+            // その他のアプリへ
+            if (checkEmpty(keyword)) return;
+
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_SEND);
+            intent.setType("text/plain");
+            intent.putExtra(Intent.EXTRA_TEXT, keyword);
             startActivity(intent);
 
         } else {
@@ -56,54 +101,17 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
     }
 
     /**
-     * アイテムが選択された時に呼ばれます。
-     *
-     * @param title タイトル選択
-     */
-    private void selectItem(String title) {
-        String keyword = mEditText.getText().toString();
-
-        if (!canSearchable(title, keyword)) {
-            return;
-        }
-
-        // 各種機能で開く
-        if (TextUtils.equals(title, getString(R.string.list_search_item_play_store))) {
-            Uri uri = Uri.parse("https://play.google.com/store/search")
-                    .buildUpon().appendQueryParameter("q", keyword).build();
-            Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-            startActivity(intent);
-
-        } else if (TextUtils.equals(title, getString(R.string.list_search_item_alarm))) {
-            Intent intent = new Intent(AlarmClock.ACTION_SET_ALARM);
-            intent.putExtra(AlarmClock.EXTRA_MESSAGE, keyword);
-            // 時間を予め適当に指定
-            intent.putExtra(AlarmClock.EXTRA_HOUR, 8); // 時
-            intent.putExtra(AlarmClock.EXTRA_MINUTES, 5); // 分
-            startActivity(intent);
-
-        } else if (TextUtils.equals(title, getString(R.string.list_search_item_misc))) {
-            Intent intent = new Intent();
-            intent.setAction(Intent.ACTION_SEND);
-            intent.setType("text/plain");
-            intent.putExtra(Intent.EXTRA_TEXT, keyword);
-            startActivity(intent);
-        }
-    }
-
-    /**
      * 検索可能かどうか調べる
      *
-     * @param title   クリックした項目のタイトル
      * @param keyword キーワード
      * @return 検索可能状態なら true
      */
-    private boolean canSearchable(String title, String keyword) {
+    private boolean checkEmpty(String keyword) {
         if (TextUtils.isEmpty(keyword)) {
             Toast.makeText(this, getString(R.string.lb_please_input_keyword), Toast.LENGTH_SHORT).show();
-            return false;
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
