b836d31d6a8ac471bb089ee9cfbe0eae6be7cc16
satoshi.kohno <s_kohno@kayosystem.com>
2015-03-26 23:32:06 +0900

    Lesson25: 不要な設定の削除とActionMode時のレイアウト崩れ修正

--- Chapter06/Lesson25/app/src/main/java/com/yokmama/learn10/chapter06/lesson25/MainActivity.java
+++ Chapter06/Lesson25/app/src/main/java/com/yokmama/learn10/chapter06/lesson25/MainActivity.java
@@ -4,13 +4,11 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.AlarmClock;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.WindowManager;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -46,23 +44,29 @@ public class MainActivity extends ActionBarActivity {
         int itemId = item.getItemId();
         try {
             if (itemId == R.id.action_send) {
-                if (checkEmpty(keyword)) return true;
+                if (checkEmpty(keyword)) {
+                    return true;
+                }
 
                 Intent intent = new Intent(Intent.ACTION_SEND);
                 intent.setType("text/plain");
                 intent.putExtra(Intent.EXTRA_TEXT, keyword);
                 startActivity(intent);
             } else if (itemId == R.id.action_google) {
-                if (checkEmpty(keyword)) return true;
+                if (checkEmpty(keyword)) {
+                    return true;
+                }
 
                 // https://www.google.co.jp/search?hl=ja&q=keyword のようなURLを構築する
                 Uri uri = Uri.parse("https://www.google.co.jp/search?hl=ja")
-                .buildUpon().appendQueryParameter("q", keyword).build();
+                        .buildUpon().appendQueryParameter("q", keyword).build();
                 Intent intent = new Intent(Intent.ACTION_VIEW, uri);
                 startActivity(intent);
             } else if (itemId == R.id.action_store) {
                 // Playストア検索
-                if (checkEmpty(keyword)) return true;
+                if (checkEmpty(keyword)) {
+                    return true;
+                }
 
                 Uri uri = Uri.parse("https://play.google.com/store/search")
                         .buildUpon().appendQueryParameter("q", keyword).build();
@@ -72,7 +76,8 @@ public class MainActivity extends ActionBarActivity {
 
             }
         } catch (ActivityNotFoundException e) {
-            Toast.makeText(this, getString(R.string.lb_activity_not_found), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.lb_activity_not_found), Toast.LENGTH_SHORT)
+                    .show();
         }
         return super.onOptionsItemSelected(item);
     }
@@ -85,7 +90,8 @@ public class MainActivity extends ActionBarActivity {
      */
     private boolean checkEmpty(String keyword) {
         if (TextUtils.isEmpty(keyword)) {
-            Toast.makeText(this, getString(R.string.lb_please_input_keyword), Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, getString(R.string.lb_please_input_keyword), Toast.LENGTH_SHORT)
+                    .show();
             return true;
         }
 
