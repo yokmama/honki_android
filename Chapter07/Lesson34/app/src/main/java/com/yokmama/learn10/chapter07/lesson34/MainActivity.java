207008853257aa1c24796388132c674024e92f3f
satoshi.kohno <s_kohno@kayosystem.com>
2015-04-08 18:10:41 +0900

    Lesson34

--- Chapter07/Lesson34/app/src/main/java/com/yokmama/learn10/chapter07/lesson34/MainActivity.java
+++ Chapter07/Lesson34/app/src/main/java/com/yokmama/learn10/chapter07/lesson34/MainActivity.java
@@ -1,19 +1,59 @@
 package com.yokmama.learn10.chapter07.lesson34;
 
+import android.animation.Animator;
+import android.animation.AnimatorInflater;
+import android.animation.ObjectAnimator;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
+
+import java.util.jar.Attributes;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements View.OnClickListener {
+
+    private GridLayout mGridLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGridLayout = (GridLayout) findViewById(R.id.grid);
+//        findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.action).setOnClickListener(this);
+
+        int maxChild = mGridLayout.getRowCount() * mGridLayout.getColumnCount();
+        for (int i = 0; i < maxChild; i++) {
+            Button button = (Button) getLayoutInflater().inflate(R.layout.activity_main_button, mGridLayout, false);
+            button.setText(i + "");
+            mGridLayout.addView(button);
+        }
     }
 
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+        if (id == R.id.action) {
+            for (int i = 0, iL = mGridLayout.getChildCount(); i < iL; i++) {
+                // 左上を基準として、
+                int vi = i % mGridLayout.getColumnCount();
+                int hi = i / mGridLayout.getRowCount();
+                int dist = Math.max(vi, hi);
+
+                View childAt = mGridLayout.getChildAt(i);
+                Animator anim = AnimatorInflater.loadAnimator(this, R.animator.ht_button);
+                anim.setTarget(childAt);
+                anim.setStartDelay(dist * 180);
+                anim.start();
+            }
+        }
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -36,4 +76,5 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
