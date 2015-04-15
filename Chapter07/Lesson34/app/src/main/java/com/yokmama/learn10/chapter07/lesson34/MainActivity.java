fbfe211c87d2e87e13f1cf48a00550ae4e0c1203
satoshi.kohno <s_kohno@kayosystem.com>
2015-04-15 17:50:59 +0900

    TranslateAnimation, Transitionを修正

--- Chapter07/Lesson34/app/src/main/java/com/yokmama/learn10/chapter07/lesson34/MainActivity.java
+++ Chapter07/Lesson34/app/src/main/java/com/yokmama/learn10/chapter07/lesson34/MainActivity.java
@@ -2,19 +2,19 @@ package com.yokmama.learn10.chapter07.lesson34;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 /**
  * Created by kayo on 15/04/15.
  */
 public class MainActivity extends ActionBarActivity {
-    private static final String ITEM_OBJECT_ANIMATION = "ObjectAnimation";
-    private static final String ITEM_TRANSLATE_ANIMATION = "TranslateAnimation";
 
     private ListView mListView;
 
@@ -26,32 +26,47 @@ public class MainActivity extends ActionBarActivity {
         mListView = (ListView) findViewById(android.R.id.list);
 
         // 一覧の作成
-        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
-        adapter.add(ITEM_OBJECT_ANIMATION);
-        adapter.add(ITEM_TRANSLATE_ANIMATION);
+        final ArrayAdapter<Activities> adapter = new ArrayAdapter<Activities>(
+                this, android.R.layout.simple_list_item_1, Activities.values());
         mListView.setAdapter(adapter);
 
         // アイテム
         mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                String item = adapter.getItem(position);
+                Activities item = adapter.getItem(position);
 
-                // クラスを取得
-                final Class<?> activityClass;
-                if (ITEM_OBJECT_ANIMATION.equals(item)) {
-                    activityClass = ObjectAnimationActivity.class;
-                } else if (ITEM_TRANSLATE_ANIMATION.equals(item)) {
-                    activityClass = TranslateAnimationActivity.class;
-                } else {
-                    throw new RuntimeException("No match item.");
+                // エラーチェック
+                if (Activities.Transition.equals(item)) {
+                    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+                        String msg = "Android Lolipop以降でのみ実行可能な項目です。";
+                        Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_SHORT).show();
+                        return;
+                    }
                 }
 
                 // Intent発行
-                Intent intent = new Intent(MainActivity.this, activityClass);
+                Intent intent = new Intent(MainActivity.this, item.activityClass);
                 startActivity(intent);
             }
         });
+    }
+
+    public enum Activities {
+        PropertyAnimation(PropertyAnimationActivity.class),
+        TranslateAnimation(TranslateAnimationActivity.class),
+        Transition(TransitionActivity.class),
+        ;
+        private final Class<? extends Activity> activityClass;
+
+        Activities(Class<? extends Activity> activityClass) {
+            this.activityClass = activityClass;
+        }
+
+        @Override
+        public String toString() {
+            return this.name();
+        }
     }
 
 }
