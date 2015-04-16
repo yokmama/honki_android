3f231168c6f8db16c8e709ef45b78077cca706f7
miyavi <gloria0021@gmail.com>
2015-04-16 16:26:41 +0900

    ・Lesson27のLollipop端末以外での動作を調整 ・Leeson32を追加

--- Chapter07/Lesson32/app/src/androidTest/java/com/yokmama/learn10/chapter07/lesson32/lesson32/ApplicationTest.java
+++ Chapter07/Lesson32/app/src/androidTest/java/com/yokmama/learn10/chapter07/lesson32/lesson32/ApplicationTest.java
@@ -1,4 +1,4 @@
-package com.yokmama.learn10.chapter07.lesson32;
+package com.yokmama.learn10.chapter07.lesson32.lesson32;
 
 import android.app.Application;
 import android.test.ApplicationTestCase;
@@ -7,6 +7,7 @@ import android.test.ApplicationTestCase;
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
+
     public ApplicationTest() {
         super(Application.class);
     }
