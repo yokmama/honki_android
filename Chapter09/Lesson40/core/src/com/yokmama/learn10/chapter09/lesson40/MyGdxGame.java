adc9d209b18af524db1efaafcc9c7ca8fd9be19b
satoshi.kohno <s_kohno@kayosystem.com>
2015-04-22 17:59:20 +0900

    Animationによる動きを実装

--- Chapter09/Lesson40/core/src/com/yokmama/learn10/chapter09/lesson40/MyGdxGame.java
+++ Chapter09/Lesson40/core/src/com/yokmama/learn10/chapter09/lesson40/MyGdxGame.java
@@ -4,8 +4,10 @@ import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.utils.Array;
 
 public class MyGdxGame extends ApplicationAdapter {
     SpriteBatch batch;
@@ -13,11 +15,24 @@ public class MyGdxGame extends ApplicationAdapter {
 
     // キャラクターのマスの大きさ指定
     final int SIZE_UNITY_CHAN = 64;
+    private Animation unityChanAnimation;
+    private float mCurrentDeltaTime;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
+
+        // テクスチャ
         img = new Texture("UnityChan.png");
+        Array<TextureRegion> unityChanKeyFrames = new Array<TextureRegion>();
+        for (int rows = 1; rows <= 2; ++rows) {
+            for (int columns = 0; columns < 4; ++columns) {
+                TextureRegion region = new TextureRegion(img, columns * SIZE_UNITY_CHAN, rows * SIZE_UNITY_CHAN, SIZE_UNITY_CHAN, SIZE_UNITY_CHAN);
+                unityChanKeyFrames.add(region);
+            }
+        }
+        float frameDuration = 0.05f;
+        unityChanAnimation = new Animation(frameDuration, unityChanKeyFrames, Animation.PlayMode.LOOP);
     }
 
     @Override
@@ -25,11 +40,14 @@ public class MyGdxGame extends ApplicationAdapter {
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        float deltaTime = Gdx.graphics.getDeltaTime();
+        mCurrentDeltaTime += deltaTime;
+
         batch.begin();
 
         // テクスチャ
-        TextureRegion textureRegion = new TextureRegion(img, 0, SIZE_UNITY_CHAN, SIZE_UNITY_CHAN, SIZE_UNITY_CHAN);
-        batch.draw(textureRegion, 0, 0);
+        TextureRegion keyFrame = unityChanAnimation.getKeyFrame(mCurrentDeltaTime);
+        batch.draw(keyFrame, 0, 0, SIZE_UNITY_CHAN * 2, SIZE_UNITY_CHAN * 2);
 
         batch.end();
     }
