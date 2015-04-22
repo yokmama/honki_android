9aa626659f70438c1e887760a762ce7e4f0753b3
satoshi.kohno <s_kohno@kayosystem.com>
2015-04-22 17:27:34 +0900

    テクスチャの張替え

--- Chapter09/Lesson40/core/src/com/yokmama/learn10/chapter09/lesson40/MyGdxGame.java
+++ Chapter09/Lesson40/core/src/com/yokmama/learn10/chapter09/lesson40/MyGdxGame.java
@@ -7,21 +7,22 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class MyGdxGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+    SpriteBatch batch;
+    Texture img;
 
-	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        img = new Texture("UnityChan.png");
+    }
+
+    @Override
+    public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(img, 0, 0);
+        batch.end();
+    }
 }
