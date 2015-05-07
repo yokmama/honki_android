0f74a551d32e8d079d4055b980c1a38157c9a9ce
masafumi terazono <hijirinhijirin@gmail.com>
2015-05-07 13:56:13 +0900

    Lesson31を修正

---
+++ Chapter07/Lesson31/before/app/src/main/java/com/yokmama/learn10/chapter07/lesson31/MainActivity.java
@@ -0,0 +1,68 @@
+package com.yokmama.learn10.chapter07.lesson31;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (getIntent() != null) {
+            if (Intent.ACTION_SEARCH.equals(getIntent().getAction())) {
+                doSearch(getIntent());
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+
+        SearchManager searchManager = (SearchManager) MainActivity.this.getSystemService(Context.SEARCH_SERVICE);
+
+        SearchView searchView = null;
+        if (searchItem != null) {
+            searchView = (SearchView) searchItem.getActionView();
+        }
+        if (searchView != null) {
+            searchView.setSearchableInfo(searchManager.getSearchableInfo(MainActivity.this.getComponentName()));
+        }
+
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onNewIntent(Intent intent) {
+        doSearch(intent);
+    }
+
+    private void doSearch(Intent intent) {
+        String queryString = intent.getStringExtra(SearchManager.QUERY);
+        Log.d(TAG, "search value:" + queryString);
+    }
+}
