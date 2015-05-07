fe5a889a4f6c18fbe15195e9cec57060253a9f0c
masafumi terazono <hijirinhijirin@gmail.com>
2015-05-07 12:13:17 +0900

    Lesson30のサンプルを修正

---
+++ Chapter07/Lesson30/before/app/src/main/java/com/yokmama/learn10/chapter07/lesson30/fragment/RadioButtonFragment.java
@@ -0,0 +1,46 @@
+package com.yokmama.learn10.chapter07.lesson30.fragment;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+
+import com.yokmama.learn10.chapter07.lesson30.R;
+
+/**
+ * Created by yokmama on 15/02/19.
+ */
+public class RadioButtonFragment extends Fragment {
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_radio_button, container, false);
+
+        //RadioGroupのインスタンスを取得
+        RadioGroup radioGroup = (RadioGroup) rootView.findViewById(R.id.radioGroup);
+
+        //リスナーをセット
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                //選択したRadioButtonのテキストを表示
+                RadioButton radioButton = (RadioButton) group.findViewById(checkedId);
+                Toast.makeText(getActivity(), radioButton.getText() + " Selected",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        return rootView;
+    }
+
+
+}
---
+++ Chapter07/Lesson30/before/app/src/main/java/com/yokmama/learn10/chapter07/lesson30/fragment/WebViewFragment.java
@@ -0,0 +1,85 @@
+package com.yokmama.learn10.chapter07.lesson30.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.widget.EditText;
+
+import com.yokmama.learn10.chapter07.lesson30.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class WebViewFragment extends Fragment {
+
+    private EditText mEditText;
+
+    private WebView mWebView;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_web_view, container, false);
+
+        //WebView,EditTextのインスタンスを取得
+        mWebView = (WebView) rootView.findViewById(R.id.webView);
+        mEditText = (EditText) rootView.findViewById(R.id.editText);
+
+        //JavaScriptをONにする
+        WebSettings webSettings = mWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        //リスナーをセット
+        rootView.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                updateWebView();
+            }
+        });
+
+        //WebViewを更新
+        updateWebView();
+
+        return rootView;
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        //WebViewを停止
+        mWebView.onPause();
+    }
+
+
+    @Override
+    public void onResume() {
+        //WebViewを再開
+        mWebView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    public void onDestroy() {
+        //WebViewを開放
+        if (mWebView != null) {
+            mWebView.destroy();
+            mWebView = null;
+        }
+        super.onDestroy();
+    }
+
+    /**
+     * WebViewを更新.
+     */
+    private void updateWebView() {
+        //入力されているURLをロード
+        String url = mEditText.getText().toString();
+        mWebView.loadUrl(url);
+    }
+
+}
