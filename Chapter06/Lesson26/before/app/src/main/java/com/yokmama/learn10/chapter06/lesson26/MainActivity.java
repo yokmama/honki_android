066c8d15c072097f35a578aa8781e1e71422126b
miyavi <gloria0021@gmail.com>
2015-05-15 18:16:53 +0900

    ・Lesson26~29のBefore,Afterを分ける

---
+++ Chapter06/Lesson26/before/app/src/main/java/com/yokmama/learn10/chapter06/lesson26/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yokmama.learn10.chapter06.lesson26;
+
+import android.app.Activity;
+import android.app.SearchManager;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
