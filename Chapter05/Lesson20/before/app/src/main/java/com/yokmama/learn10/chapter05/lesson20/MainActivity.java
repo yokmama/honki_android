e49de0f3604a009be2eaf6814ec124a5d4c932bd
miyavi <gloria0021@gmail.com>
2015-05-21 18:03:56 +0900

    Lesson16~22までのサンプルプロジェクトのbuild.prop/Activityの継承を修正

--- Chapter05/Lesson20/before/app/src/main/java/com/yokmama/learn10/chapter05/lesson20/MainActivity.java
+++ Chapter05/Lesson20/before/app/src/main/java/com/yokmama/learn10/chapter05/lesson20/MainActivity.java
@@ -1,12 +1,9 @@
 package com.yokmama.learn10.chapter05.lesson20;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- Chapter05/Lesson21/after/app/src/main/java/com/yokmama/learn10/chapter05/lesson21/MainActivity.java
+++ Chapter05/Lesson21/after/app/src/main/java/com/yokmama/learn10/chapter05/lesson21/MainActivity.java
@@ -1,12 +1,12 @@
 package com.yokmama.learn10.chapter05.lesson21;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.View;
 
 
-public class MainActivity extends FragmentActivity implements View.OnClickListener {
+public class MainActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
