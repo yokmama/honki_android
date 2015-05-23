d5972f673d8ff684e77ea1e172aa5320fa130001
satoshi.kohno <s_kohno@kayosystem.com>
2015-05-23 21:37:15 +0900

    クリアアニメーション追加

--- Chapter09/Lesson41/after/core/src/com/yokmama/learn10/chapter09/lesson41/Hero.java
+++ Chapter09/Lesson41/after/core/src/com/yokmama/learn10/chapter09/lesson41/Hero.java
@@ -17,13 +17,15 @@ class Hero {
     private static final int TEXTURE_TILE_HEIGHT = 64;
 
     // ジャンプ時間
-    private static final float TIME_DURATION_JUMPING = 0.05f;
     private static final float TIME_DURATION_RUNNING = 0.05f;
+    private static final float TIME_DURATION_JUMPING = 0.05f;
+    private static final float TIME_DURATION_WIN = 0.2f;
 
     // アニメーションの状態
     private final static int ANIM_STATE_STILL = -1;
     private final static int ANIM_STATE_RUNNING = 0;
     private final static int ANIM_STATE_JUMPING = 1;
+    private final static int ANIM_STATE_WIN = 2;
 
     // 地面からの距離
     static final float HERO_FLOOR_Y = 45;
@@ -55,9 +57,20 @@ class Hero {
     // キャラクタの衝突判定用範囲オブジェクト
     private Rectangle mCollisionRect = new Rectangle();
 
-    // フラグ
+    // ゲームオーバー
     boolean mIsDead;
+    float mDeadTime;
+    float mDeadPosY;
+
+    // ゲームクリア
+    final static int WIN_ANIM_STATE_WAIT_FOR_LANDING = 0;
+    final static int WIN_ANIM_STATE_RUN = 1;
+    final static int WIN_ANIM_STATE_JUMP = 2;
+    final static int WIN_ANIM_STATE_SPIRAL = 3;
+    final static int WIN_ANIM_STATE_STATIC = 4;
     boolean mHasWon;
+    int mWinAnimState;
+    float mWinAnimStateTime;
 
     // キャラクターの速度
     float heroVelocityX = 400;
@@ -98,6 +111,11 @@ class Hero {
         regions.addAll(split[3], 0, 7);
         sAnimations[ANIM_STATE_JUMPING] = new Animation(TIME_DURATION_JUMPING,
                 regions, Animation.PlayMode.NORMAL);
+
+        regions = new Array<TextureRegion>();
+        regions.addAll(split[0], 3, 2);
+        sAnimations[ANIM_STATE_WIN] = new Animation(TIME_DURATION_WIN,
+                regions, Animation.PlayMode.LOOP);
 
         sDeadFrame = split[0][3];
     }
@@ -154,6 +172,33 @@ class Hero {
             }
         }
 
+        // Win animation
+        if (mHasWon) {
+            if (!mIsJumping) {
+                if (mWinAnimState == WIN_ANIM_STATE_WAIT_FOR_LANDING) {
+                    mWinAnimState = WIN_ANIM_STATE_RUN;
+                    mAnimState = ANIM_STATE_RUNNING;
+                    mCurrentStateDisplayTime = 0;
+                }
+                else if (mWinAnimState == WIN_ANIM_STATE_RUN) {
+                    if (mCurrentStateDisplayTime > 0.4f) {
+                        mWinAnimState = WIN_ANIM_STATE_SPIRAL;
+                        mAnimState = ANIM_STATE_WIN;
+                        mCurrentStateDisplayTime = 0;
+                        mVelocity.set(0, 0);
+                    }
+                }
+                else if (mWinAnimState == WIN_ANIM_STATE_SPIRAL) {
+                    if (mCurrentStateDisplayTime > 1.f) {
+                        mWinAnimState = WIN_ANIM_STATE_STATIC;
+                        mAnimState = ANIM_STATE_STILL;
+                        mAnimStillFrame = sAnimations[ANIM_STATE_WIN].getKeyFrame(2);
+                        mCurrentStateDisplayTime = 0;
+                    }
+                }
+            }
+        }
+
         mPosition.mulAdd(mVelocity, deltaTime);
 
         mCollisionRect.set(mPosition.x + 30, mPosition.y, 40, 68);
@@ -187,7 +232,9 @@ class Hero {
     // ゲームクリア通知
     public void win() {
         mHasWon = true;
-        // TODO: ゴール時
+        mWinAnimState = WIN_ANIM_STATE_WAIT_FOR_LANDING;
+        mWinAnimStateTime = 0;
+        mVelocity.set(300, 0);
     }
 
     // ゲームオーバー通知
@@ -195,7 +242,8 @@ class Hero {
         mIsDead = true;
         mAnimStillFrame = sDeadFrame;
         mAnimState = ANIM_STATE_STILL;
-        // TODO: ゲームオーバー時
+        mDeadTime = 0.0f;
+        mDeadPosY = mPosition.y;
     }
 
 }
