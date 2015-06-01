08b8b1756ec2c41ed0cb9a9db4bb3d73cd2324b0
satoshi.kohno <s_kohno@kayosystem.com>
2015-06-01 16:26:20 +0900

    Lesson34
    ・after/before調整

--- Chapter07/Lesson34/before/app/src/main/java/com/yokmama/learn10/chapter07/lesson34/ui/TranslateAnimationDialogFragment.java
+++ Chapter07/Lesson34/before/app/src/main/java/com/yokmama/learn10/chapter07/lesson34/ui/TranslateAnimationDialogFragment.java
@@ -1,8 +1,10 @@
 package com.yokmama.learn10.chapter07.lesson34.ui;
 
+import android.annotation.TargetApi;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.DialogInterface;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
 
@@ -11,12 +13,13 @@ import com.yokmama.learn10.chapter07.lesson34.R;
 /**
  * Created by kayo on 15/04/17.
  */
+@TargetApi(Build.VERSION_CODES.HONEYCOMB)
 public class TranslateAnimationDialogFragment extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder bld = new AlertDialog.Builder(
-                getActivity());
+                getActivity(), R.style.Lesson34_Animation_Dialog);
         bld.setTitle("タイトル");
         bld.setMessage("メッセージ");
         bld.setPositiveButton("OK", new DialogInterface.OnClickListener() {
