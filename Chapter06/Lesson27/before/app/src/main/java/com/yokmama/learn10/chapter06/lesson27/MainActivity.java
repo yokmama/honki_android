9fbe5b08092fb386d5fa56d1def73402c0bb6829
miyavi <gloria0021@gmail.com>
2015-06-17 10:07:52 +0900

    ・GridLayoutのimportでsupportライブラリを優先させるためにimport文を追加

--- Chapter06/Lesson27/before/app/src/main/java/com/yokmama/learn10/chapter06/lesson27/MainActivity.java
+++ Chapter06/Lesson27/before/app/src/main/java/com/yokmama/learn10/chapter06/lesson27/MainActivity.java
@@ -6,6 +6,7 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.support.v7.widget.GridLayout;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
